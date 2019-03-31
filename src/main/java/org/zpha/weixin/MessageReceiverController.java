7903d242eed03c4ce1480d2abe99c6eb0e08ab1e
辉仔 <3524748062@qq.com>
2019-03-31 16:46:44 +0800

    Revert "增加请求参数和方法参数的映射"
    This reverts commit 89451c8820115bb3d197dab2810493d9751890cf.

--- src/main/java/org/zpha/weixin/MessageReceiverController.java
+++ src/main/java/org/zpha/weixin/MessageReceiverController.java
@@ -2,17 +2,16 @@ package org.zpha.weixin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/zph/weixin/receiver")
 public class MessageReceiverController {
 @GetMapping
 public String echo( 
-	@RequestParam("signature") String signature,
-	@RequestParam("timestamp") String timestamp,
-	@RequestParam("nonce") String nonce,
-	@RequestParam("echostr") String echostr
+	String signature,
+	String timestamp,
+	String nonce,
+	String echostr
 	){
 		
 	
