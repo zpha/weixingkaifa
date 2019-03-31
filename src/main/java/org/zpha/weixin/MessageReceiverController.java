89451c8820115bb3d197dab2810493d9751890cf
辉仔 <3524748062@qq.com>
2019-03-31 16:42:32 +0800

    增加请求参数和方法参数的映射

--- src/main/java/org/zpha/weixin/MessageReceiverController.java
+++ src/main/java/org/zpha/weixin/MessageReceiverController.java
@@ -2,16 +2,17 @@ package org.zpha.weixin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/zph/weixin/receiver")
 public class MessageReceiverController {
 @GetMapping
 public String echo( 
-	String signature,
-	String timestamp,
-	String nonce,
-	String echostr
+	@RequestParam("signature") String signature,
+	@RequestParam("timestamp") String timestamp,
+	@RequestParam("nonce") String nonce,
+	@RequestParam("echostr") String echostr
 	){
 		
 	
