890491f8d13c8df00230a92229156821cef17c2c
辉仔 <3524748062@qq.com>
2019-04-01 21:41:30 +0800

    ddddddd

--- src/main/java/org/zpha/weixin/WeixinApplication.java
+++ src/main/java/org/zpha/weixin/WeixinApplication.java
@@ -1,10 +1,24 @@
 package org.zpha.weixin;
 
+import org.zpha.weixin.domain.InMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class WeixinApplication {
+
+	// 相当于Spring的XML配置方式中的<bean>元素
+	@Bean
+	public RedisTemplate<String, InMessage> inMessageTemplate(//
+			@Autowired RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, InMessage> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeixinApplication.class, args);
