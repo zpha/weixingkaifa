2b2c7dec8a4166ef4255436d877c24f1063fe9c0
辉仔 <3524748062@qq.com>
2019-04-02 18:50:46 +0800

    dddddd

--- src/main/java/org/zpha/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/zpha/weixin/controller/MessageReceiverController.java
@@ -32,11 +32,11 @@ public class MessageReceiverController {
 	private RedisTemplate<String, InMessage> inMessageTemplate;
 
 	@GetMapping // 只处理GET请求
-	public String echo(//
-			@RequestParam("signature") String signature, //
-			@RequestParam("timestamp") String timestamp, //
-			@RequestParam("nonce") String nonce, //
-			@RequestParam("echostr") String echostr//
+	public String echo(
+			@RequestParam("signature") String signature, 
+			@RequestParam("timestamp") String timestamp, 
+			@RequestParam("nonce") String nonce, 
+			@RequestParam("echostr") String echostr
 	) {
 		// 正常来讲，需要把timestamp和nonce放入一个数组，并进行排序
 		// 接着把排序后的两个元素拼接成一个新的String
@@ -89,7 +89,7 @@ public class MessageReceiverController {
 					// 比如文本消息、图片消息处理方式不同，所以使用前缀来隔离： text* 表示文本消息、image* 表示图片消息。
 					// 建议在多人共享一个服务器的时候，每个人使用不同的数据库实例即可，并且建议在通道名称之前加上反向代理的前缀。
 
-					String channel = "zph_" + inMessage.getMsgType();
+					String channel = "zpha_" + inMessage.getMsgType();
 
 					// 消息内容要自己序列化才能放入队列中
 					ByteArrayOutputStream out = new ByteArrayOutputStream();// 输出流
